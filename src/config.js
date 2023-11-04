fec5f007b5febe57ef3e7cb8b65da651d652df24
Sabari7373 <sabariganesh7373@gmail.com>
2023-11-04 13:44:08 +0530

    link changed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const Config = {
   // api:"http://localhost:3001"
     //  api:"https://zenclass-backend-ne81te04g-sabaris-projects.vercel.app"
-    api:'https://student-dashboard-backend-cgeq.onrender.com'
+    api:'https://zenclass-backend-1oyr.onrender.com'
 };
